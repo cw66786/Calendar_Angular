406cb17ad246a6f90a69dfdefed7274079cb2a7d
Craig Wilson <cw66786@gmail.com>
2023-03-07 15:15:13 -0600

    basic display done

--- calendar/src/app/calendar/calendar.component.ts
+++ calendar/src/app/calendar/calendar.component.ts
@@ -12,12 +12,12 @@ export class CalendarComponent implements OnInit {
   weekdays: string[];
   months: string[];
   days: Date[];
+  currentDate = new Date();
 
 
   constructor(service: CalendarService) { 
-    let currentDate = new Date();
    [this.weekdays,this.months] = [service.weekdays,service.months];
-   this.days = service.getDaysInMonth(currentDate.getMonth(),currentDate.getFullYear());
+   this.days = service.getDaysInMonth(this.currentDate.getMonth(),this.currentDate.getFullYear());
 
   }
 
