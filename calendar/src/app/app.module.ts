bb497927b5a369faff1370c48e0b2c8a63217d89
Craig Wilson <cw66786@gmail.com>
2023-03-07 14:26:20 -0600

    buttons set up

--- calendar/src/app/app.module.ts
+++ calendar/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
