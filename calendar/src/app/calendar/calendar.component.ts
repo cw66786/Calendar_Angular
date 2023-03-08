863b7e215858fb53b9ff901c678e1cfb9b35162d
Craig Wilson <cw66786@gmail.com>
2023-03-08 16:07:39 -0600

    changing months fixed

--- calendar/src/app/calendar/calendar.component.ts
+++ calendar/src/app/calendar/calendar.component.ts
@@ -1,42 +1,87 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarService } from './calendar.service';
 
-
-
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss']
+  styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent implements OnInit {
-  weekdays: string[];
-  months: string[];
-  days: Date[];
+  weekdays: string[] = [];
+  months: string[] = [];
+  days: Date[] = [];
   currentDate = new Date();
-  emptyDays: string[];
-  endingEmpties: string[];
+  emptyDays: string[] = [];
+  endingEmpties: string[] = [];
 
-
-  constructor(service: CalendarService) { 
-   [this.weekdays,this.months] = [service.weekdays,service.months];
-   this.days = service.getDaysInMonth(this.currentDate.getMonth(),this.currentDate.getFullYear());
-    this.emptyDays = service.getStartOfMonth(this.days);
-    this.endingEmpties = service.getEndOfMonth(this.days,this.emptyDays);
+  constructor(private service: CalendarService) {
+    [this.weekdays, this.months] = [this.service.weekdays, this.service.months];
   }
-
-  ngOnInit(): void {
-  }
-
-
   
-isWeekend(day: Date){
-  if(day.getDay() === 0 || day.getDay() === 6 ){
-    return true;
+  ngOnInit(): void {
+    this.days = this.service.getDaysInMonth(
+      this.currentDate.getMonth(),
+      this.currentDate.getFullYear()
+    );
+   this.updateEmpties();
   }
-  return false;
- 
 
-}
+  isWeekend(day: Date) {
+    if (day.getDay() === 0 || day.getDay() === 6) {
+      return true;
+    }
+    return false;
+  }
+
+  updateEmpties(){
+    this.emptyDays = this.service.getStartOfMonth(this.days);
+    this.endingEmpties = this.service.getEndOfMonth(this.days, this.emptyDays);
+  }
+
+  nextMonth() {
+    
+    if (this.days[0].getMonth() < 11) {
+      this.currentDate.setMonth(this.currentDate.getMonth() + 1);
+      this.days = this.service.getDaysInMonth(
+        (this.currentDate.getMonth()),
+        this.currentDate.getFullYear()
+        );
+        this.updateEmpties();
+        
+      } else if (this.days[0].getMonth() === 11) {
+      this.currentDate.setFullYear(this.currentDate.getFullYear() + 1);
+      this.days = this.service.getDaysInMonth(
+        (this.currentDate.getMonth() - 9),
+        this.currentDate.getFullYear() 
+      );
+   this.updateEmpties();
+
+    }
+  }
+
+  previousMonth() {
+    
+    if (this.days[0].getMonth() > 0) {
+      this.currentDate.setMonth(this.currentDate.getMonth()-1);
+      this.days = this.service.getDaysInMonth(
+        (this.currentDate.getMonth()),
+        this.currentDate.getFullYear()
+        );
+        this.updateEmpties();
+        
+      } else if (this.days[0].getMonth() === 0) {
+      
+      this.currentDate.setFullYear(this.currentDate.getFullYear()-1);
+
+      this.days = this.service.getDaysInMonth(
+        (this.currentDate.getMonth() + 9),
+        (this.currentDate.getFullYear())
+      );
+   this.updateEmpties();
+
+    }
+  }
+
 
 
 }
--- calendar/src/app/calendar/calendar.service.ts
+++ calendar/src/app/calendar/calendar.service.ts
@@ -1,75 +1,66 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalendarService {
-
-  weekdays: string[] = ["Sun","Mon","Tues","Wed","Thur","Fri","Sat"];
+  weekdays: string[] = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
   months: string[] = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
   ];
-  
 
-  constructor() { }
-  
+  constructor() {}
 
-   getDaysInMonth = (month: number, year: number)=> {
+  getDaysInMonth = (month: number, year: number) => {
     let date = new Date(year, month, 1);
     let days: Date[] = [];
-   
+
     while (date.getMonth() === month) {
       days.push(new Date(date));
       date.setDate(date.getDate() + 1);
     }
 
-    
-    
-     return days;
-  }
+    return days;
+  };
 
-
-  getStartOfMonth(days: Date []){
+  getStartOfMonth(days: Date[]) {
     let emptyDays: string[] = [];
 
     let weekStart: any = days[0].getDay();
-    
-    if(weekStart > 0){
-      for(let i = weekStart; i > 0; i--){
-          emptyDays.push('');
+
+    if (weekStart > 0) {
+      for (let i = weekStart; i > 0; i--) {
+        emptyDays.push('');
       }
     }
 
     return emptyDays;
   }
 
-
-  getEndOfMonth(days: Date[],empty: string []){
+  getEndOfMonth(days: Date[], empty: string[]) {
     let total: number = days.length + empty.length;
 
-    let leftOvers: number = 35 - total;
+    let leftOvers: number = total > 35 ? 42 - total : 35 - total;
 
     let endingEmpties: string[] = [];
 
-    if(leftOvers > 0){
-      for(let i = leftOvers; i > 0; i--){
-          endingEmpties.push('');
+    if (leftOvers > 0) {
+      for (let i = leftOvers; i > 0; i--) {
+        endingEmpties.push('');
       }
-    
     }
-    
+
     return endingEmpties;
   }
-
 }
